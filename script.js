74fa5433ac2d790e74a2345eeb3b4a5438c10f50
Glitch (wikipedia-revealer) <none>
2018-04-14 21:00:49 +0000

    ✨🎽 Checkpoint ./script.js:641133/27

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* global Reveal:false */
 (async () => {
+  $('.mw-parser-output');
+
   await $.getScript('https://cdn.jsdelivr.net/npm/reveal.js@3.6.0/js/reveal.min.js');
   
   Reveal.initialize();
