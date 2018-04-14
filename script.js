3f48ea96e5354d0dfae6882d57bc39e7daf5359c
Glitch (wikipedia-revealer) <none>
2018-04-14 19:36:40 +0000

    🛰🍣 Checkpoint ./script.js:641133/54 ./index.html:641133/811

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+/* global Reveal:false */
 (() => {
   $('<script/>')
     .attr('src', 'https://cdn.jsdelivr.net/npm/reveal.js@3.6.0/js/reveal.min.js')
     .appendTo(document.head);
   
-  
+  Reveal.initialize();
 })();
