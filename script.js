5db8b94332f359cab307969543e5947799ed1e41
Glitch (wikipedia-revealer) <none>
2018-04-14 19:57:23 +0000

    🍤🍋 Checkpoint ./script.js:641133/87

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 /* global Reveal:false */
-(() => {
-  $('<script/>')
-    .attr('src', 'https://cdn.jsdelivr.net/npm/reveal.js@3.6.0/js/reveal.min.js')
-    .appendTo(document.head);
+(async () => {
+  await $.getScript('https://cdn.jsdelivr.net/npm/reveal.js@3.6.0/js/reveal.min.js');
   
   Reveal.initialize();
 })();
