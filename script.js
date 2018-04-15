c2a138378559d3d5191f48c3690dc9d15ebc8c4c
Glitch (wikipedia-revealer) <none>
2018-04-15 06:45:13 +0000

    📙⏳ Checkpoint ./script.js:641133/232

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 /* global Reveal:false */
 (async () => {
-  $('.mw-parser-output')
-    .addClass('slides');
-
   $('#mw-content-text')
     .addClass('reveal')
     .prependTo(document.body)
     .nextAll()
     .remove();
+
+  $('.mw-parser-output')
+    .addClass('slides')
+    .nextAll()
+    .remove();
+
+  $('.vertical-navbox').remove();
 
   $('<link rel="stylesheet"/>')
     .attr('href', 'https://cdn.jsdelivr.net/npm/reveal.js@3.0.0/css/reveal.min.css')
