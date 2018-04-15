fec96be98828e907f4bb147d7c8dfa04d52f0e55
Glitch (wikipedia-revealer) <none>
2018-04-15 14:56:29 +0000

    🏀💗 Checkpoint ./script.js:641133/671

--- script.js
+++ script.js
@@ -18,16 +18,16 @@
 
   $('.vertical-navbox, .mw-editsection').remove();
 
-  $('
+  $('p').addClass('fragment');
+
   $('<link rel="stylesheet"/>')
     .attr('href', 'https://cdn.jsdelivr.net/npm/reveal.js@3.0.0/css/reveal.min.css')
     .appendTo(document.head);
 
   $('<link rel="stylesheet"/>')
-    .attr('href', 'https://cdn.jsdelivr.net/npm/reveal.js@3.6.0/css/theme/black.min.css')
+    .attr('href', 'https://cdn.jsdelivr.net/npm/reveal.js@3.6.0/css/theme/league.min.css')
     .appendTo(document.head);
 
-  await $.getScript('https://cdn.jsdelivr.net/npm/reveal.js@3.6.0/lib/js/head.min.js');
   await $.getScript('https://cdn.jsdelivr.net/npm/reveal.js@3.6.0/js/reveal.min.js');
   
   Reveal.initialize();
