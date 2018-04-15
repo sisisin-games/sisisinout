ccfb0265e2f7b81512067f9ee38f0d7129c26867
Glitch (wikipedia-revealer) <none>
2018-04-15 00:14:14 +0000

    ⭐️🚉 Checkpoint ./script.js:641133/1

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
   $('<link rel="stylesheet"/>')
     .attr('href', 'https://cdn.jsdelivr.net/npm/reveal.js@3.0.0/css/reveal.min.css')
     .appendTo(document.head);
+
   await $.getScript('https://cdn.jsdelivr.net/npm/reveal.js@3.6.0/js/reveal.min.js');
   
   Reveal.initialize();
