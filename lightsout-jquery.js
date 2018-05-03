673b2944d3ed183be7a1962f5e0a26b93b5b6663
Glitch (lightsout-sample) <none>
2018-05-03 11:18:02 +0000

    🏏🕶 Checkpoint ./lightsout-jquery.js:641133/1549

--- lightsout-jquery.js
+++ lightsout-jquery.js
@@ -1,3 +1,16 @@
+$(document).on('click', '.light', ({target}) => {
+  const $l = $(target);
+  const x = +$l.data('x');
+  const y = +$l.data('y');
+
+  $l
+    .add(`[data-x="${x}"][data-y="${y - 1}"]`)
+    .add(`[data-x="${x - 1}"][data-y="${y}"]`)
+    .add(`[data-x="${x + 1}"][data-y="${y}"]`)
+    .add(`[data-x="${x}"][data-y="${y + 1}"]`)
+    .toggleClass('on');
+});
+
 jQuery(async $ => {
   const [, width, height] = (location.search.match(/\bsize=(\d+)x(\d+)/) || [, 5, 5]).map(Number);
   const board = $('.board').css({
@@ -7,11 +20,11 @@ jQuery(async $ => {
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      board.append(`<div class="light" data-x="${x}" data-y="${y}">`);
+      const l = $(`<div class="light" data-x="${x}" data-y="${y}">`);
+      board.append(l);
+
+      if (Math.random() < 0.5)
+        l.click();
     }
   }
-
-  $('.light').each(function() {
-    $(this).toggleClass('on', Math.random() < 0.5);
-  });
 });
