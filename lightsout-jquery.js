51b5357461e8f1959ebe3eb339db4daa9efa9a62
Glitch (lightsout-sample) <none>
2018-05-03 11:07:38 +0000

    🎭🍚 Checkpoint ./lightsout.css:641133/389 ./lightsout-jquery.js:641133/314

--- lightsout-jquery.js
+++ lightsout-jquery.js
@@ -1,10 +1,17 @@
 jQuery(async $ => {
   const [, width, height] = (location.search.match(/\bsize=(\d+)x(\d+)/) || [, 5, 5]).map(Number);
-  const board = $('.board');
+  const board = $('.board').css({
+    'grid-template-columns': '1fr '.repeat(width),
+    'grid-template-rows': '1fr '.repeat(height),
+  });
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
       board.append(`<div class="light" data-x="${x}" data-y="${y}">`);
     }
   }
+
+  $('.light').each(function() {
+    $(this).toggleClass('on', Math.random() < 0.5);
+  });
 });
