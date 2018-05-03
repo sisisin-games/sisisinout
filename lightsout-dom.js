1a51fc51a771779ced041d72013ad5d87f30ef18
Glitch (lightsout-sample) <none>
2018-05-03 13:15:06 +0000

    💾🌯 Checkpoint ./lightsout-dom.js:641133/1433 ./jquery.html:641133/5 ./dom.html:641133/79

--- lightsout-dom.js
+++ lightsout-dom.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', async () => {
   const [, width, height] = (location.search.match(/\bsize=(\d+)x(\d+)/) || [, 5, 5]).map(Number);
 
-  document.addEventListener('click', ({currentTarget}) => {
-    if (!currentTarget.matches('.light'))
+  document.addEventListener('click', ({target}) => {
+    if (!target.matches('.light'))
       return;
 
-    const x = +currentTarget.getAttribute('data-x');
-    const y = +currentTarget.getAttribute('data-y');
+    onClick(
+      +target.getAttribute('data-x'),
+      +target.getAttribute('data-y'),
+    );
+  }, false);
 
+  function onClick(x, y) {
     document.querySelectorAll(`
       [data-x="${x}"][data-y="${y}"],
       [data-x="${x}"][data-y="${y - 1}"],
@@ -17,9 +21,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     `).forEach(el => el.classList.toggle('on'));
 
     setTimeout(onChange, 100);
-  }, false);
-
-  function onClick(x, y) {
   }
 
   function onChange() {
@@ -28,28 +29,25 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 
   function onFinish() {
+    const endedAt = Date.now();
+
+    alert(`クリア！\nタイムは ${(endedAt - startedAt) / 1000 | 0} 秒でした`);
+
+    location.href = `?size=${width + 1}x${height + 1}`;
   }
 
-
   const board = document.querySelector('.board');
+
   board.style['grid-template-columns'] = '1fr '.repeat(width);
   board.style['grid-template-rows'] = '1fr '.repeat(height);
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
+  for (let y = 0; y < height; y++)
+    for (let x = 0; x < width; x++)
       board.insertAdjacentHTML('beforeend', `<div class="light" data-x="${x}" data-y="${y}">`);
-    }
-  }
 
   Array.from(document.querySelectorAll('.light'))
     .filter(() => Math.random() < 0.5)
     .forEach(el => el.click());
 
   const startedAt = Date.now();
-
-  await new Promise(resolve => $(document).one('lo:finish', resolve));
-
-  const endedAt = Date.now();
-
-  alert(`クリア！\nタイムは ${(endedAt - startedAt) / 1000 | 0} 秒でした`);
 }, false);
