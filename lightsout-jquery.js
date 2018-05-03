2075f0584d2f501f2462b61675f59502f5607760
Glitch (lightsout-sample) <none>
2018-05-03 10:57:05 +0000

    🌖🌪 Checkpoint ./lightsout-jquery.js:641133/399 ./jquery.html:641133/49

--- lightsout-jquery.js
+++ lightsout-jquery.js
@@ -1,5 +1,10 @@
 jQuery(async $ => {
-  const [width, height] = (() => {
-    location.search.
-  })();
+  const [, width, height] = (location.search.match(/\bsize=(\d+)x(\d+)/) || [, 5, 5]).map(Number);
+  const board = $('.board');
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      board.append(`<div class="light" data-x="${x}" data-y="${y}">`);
+    }
+  }
 });
