4c5e9db224c56e08e422831a3c7e97cda67a4181
Glitch (lightsout-sample) <none>
2018-05-03 13:04:35 +0000

    🏇🌓 Checkpoint ./lightsout-dom.js:641133/5521

--- lightsout-dom.js
+++ lightsout-dom.js
@@ -1,42 +1,49 @@
-document.addEventListener('click', ({currentTarget}) => {
-  if (current)
-
-  $(document).trigger('lo:click', [
-    +$l.data('x'),
-    +$l.data('y'),
-  ]);
-}, false);
-
-$(document).on('lo:click', (_, x, y) => {
-  $(`[data-x="${x}"][data-y="${y}"]`)
-    .add(`[data-x="${x}"][data-y="${y - 1}"]`)
-    .add(`[data-x="${x - 1}"][data-y="${y}"]`)
-    .add(`[data-x="${x + 1}"][data-y="${y}"]`)
-    .add(`[data-x="${x}"][data-y="${y + 1}"]`)
-    .toggleClass('on');
-
-  setTimeout(() => $(document).trigger('lo:change'), 100);
-});
-
-$(document).on('lo:change', () => {
-  if (!$('.light.on').length)
-    $(document).trigger('lo:finish');
-});
-
-jQuery(async $ => {
+document.addEventListener('DOMContentLoaded', async () => {
   const [, width, height] = (location.search.match(/\bsize=(\d+)x(\d+)/) || [, 5, 5]).map(Number);
-  const board = $('.board').css({
-    'grid-template-columns': '1fr '.repeat(width),
-    'grid-template-rows': '1fr '.repeat(height),
-  });
+
+  document.addEventListener('click', ({currentTarget}) => {
+    if (!currentTarget.matches('.light'))
+      return;
+
+    const x = +currentTarget.getAttribute('data-x');
+    const y = +currentTarget.getAttribute('data-y');
+
+    document.querySelectorAll(`
+      [data-x="${x}"][data-y="${y}"],
+      [data-x="${x}"][data-y="${y - 1}"],
+      [data-x="${x - 1}"][data-y="${y}"],
+      [data-x="${x + 1}"][data-y="${y}"],
+      [data-x="${x}"][data-y="${y + 1}"]
+    `).forEach(el => el.classList.toggle('on'));
+
+    setTimeout(onChange, 100);
+  }, false);
+
+  function onClick(x, y) {
+  }
+
+  function onChange() {
+    if (!document.querySelector('.light.on'))
+      onFinish();
+  }
+
+  function onFinish() {
+  }
+
+
+  const board = document.querySelector('.board');
+  board.style['grid-template-columns'] = '1fr '.repeat(width);
+  board.style['grid-template-rows'] = '1fr '.repeat(height);
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      board.append(`<div class="light" data-x="${x}" data-y="${y}">`);
+      board.insertAdjacentHTML('beforeend', `<div class="light" data-x="${x}" data-y="${y}">`);
     }
   }
 
-  $('.light').filter(() => Math.random() < 0.5).each((_, l) => $(l).click());
+  Array.from(document.querySelectorAll('.light'))
+    .filter(() => Math.random() < 0.5)
+    .forEach(el => el.click());
 
   const startedAt = Date.now();
 
@@ -45,7 +52,4 @@ jQuery(async $ => {
   const endedAt = Date.now();
 
   alert(`クリア！\nタイムは ${(endedAt - startedAt) / 1000 | 0} 秒でした`);
-});
-
-document.addEventListener('DOMContentLoaded', async () => {
 }, false);
