5a7148cf62c796b0ee7c251f079dbd948cc3627c
Glitch (lightsout-sample) <none>
2018-05-03 12:53:30 +0000

    🎁👩‍👩‍👦 Checkpoint ./lightsout.css:641133/88 ./lightsout-dom.js:641133/4071

--- lightsout-dom.js
+++ lightsout-dom.js
@@ -1,2 +1,51 @@
+document.addEventListener('click', ({currentTarget}) => {
+  if (current)
+
+  $(document).trigger('lo:click', [
+    +$l.data('x'),
+    +$l.data('y'),
+  ]);
+}, false);
+
+$(document).on('lo:click', (_, x, y) => {
+  $(`[data-x="${x}"][data-y="${y}"]`)
+    .add(`[data-x="${x}"][data-y="${y - 1}"]`)
+    .add(`[data-x="${x - 1}"][data-y="${y}"]`)
+    .add(`[data-x="${x + 1}"][data-y="${y}"]`)
+    .add(`[data-x="${x}"][data-y="${y + 1}"]`)
+    .toggleClass('on');
+
+  setTimeout(() => $(document).trigger('lo:change'), 100);
+});
+
+$(document).on('lo:change', () => {
+  if (!$('.light.on').length)
+    $(document).trigger('lo:finish');
+});
+
+jQuery(async $ => {
+  const [, width, height] = (location.search.match(/\bsize=(\d+)x(\d+)/) || [, 5, 5]).map(Number);
+  const board = $('.board').css({
+    'grid-template-columns': '1fr '.repeat(width),
+    'grid-template-rows': '1fr '.repeat(height),
+  });
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      board.append(`<div class="light" data-x="${x}" data-y="${y}">`);
+    }
+  }
+
+  $('.light').filter(() => Math.random() < 0.5).each((_, l) => $(l).click());
+
+  const startedAt = Date.now();
+
+  await new Promise(resolve => $(document).one('lo:finish', resolve));
+
+  const endedAt = Date.now();
+
+  alert(`クリア！\nタイムは ${(endedAt - startedAt) / 1000 | 0} 秒でした`);
+});
+
 document.addEventListener('DOMContentLoaded', async () => {
 }, false);
