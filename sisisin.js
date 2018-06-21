2d2ef9dbafce59c5cb9115f178d45e2fd61cfde4
Glitch (sisisinout) <none>
2018-06-21 17:09:37 +0000

    🌓🚩 Checkpoint ./sisisin.js:641133/803 ./sisisin.css:641133/73 ./index.html:641133/31

--- sisisin.js
+++ sisisin.js
@@ -9,19 +9,19 @@ $(document).on('click', '.nyan', ({target}) => {
 
 $(document).on('si:click', (_, x, y) => {
   $(`[data-x="${x}"][data-y="${y}"]`).each((_, nyan) => {
-    const $nyan = $(nyan);
-    const deg = $nyan.data('deg') + 90;
+    const deg = +nyan.dataset.deg + 90;
     nyan.dataset.r = (+nyan.dataset.r + 1) % 4;
-    $nyan.data('deg', deg).css({'transform': `rotate(${deg}deg)`});
+    nyan.dataset.deg = deg;
+    nyan.style.transform = `rotate(${deg}deg)`;
   });
   $(`
     [data-x="${x}"][data-y="${y - 1}"], [data-x="${x - 1}"][data-y="${y}"],
     [data-x="${x + 1}"][data-y="${y}"], [data-x="${x}"][data-y="${y + 1}"]
   `).each((_, nyan) => {
-    const $nyan = $(nyan);
-    const deg = $nyan.data('deg') - 90;
+    const deg = nyan.dataset.deg - 90;
     nyan.dataset.r = (+nyan.dataset.r + 3) % 4;
-    $nyan.data('deg', deg).css({'transform': `rotate(${deg}deg)`});
+    nyan.dataset.deg = deg;
+    nyan.style.transform = `rotate(${deg}deg)`;
   });
 
   setTimeout(() => $(document).trigger('si:change'), 200);
@@ -41,7 +41,7 @@ jQuery(async $ => {
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      board.append(`<div class="nyan" data-x="${x}" data-y="${y}" data-r="0">`).data('deg', 0);
+      board.append(`<div class="nyan" data-x="${x}" data-y="${y}" data-r="0" data-deg="0">`);
     }
   }
 
